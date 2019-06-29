26bae8388293b65cd0ea626296c786ac80b3181c
David Carracedo <david@adaformacion.es>
2019-06-29 19:04:41 +0200

    Added sample form and basic POST funcionality

--- app.py
+++ app.py
@@ -12,19 +12,18 @@ def inicio():
 def script1():
     sc1=Script1()
     resultado=sc1.funcion1()
-    return render_template('inicio.html', resultado=resultado)
+    return render_template('script1.html', resultado=resultado)
 
 @app.route('/script2')
 def script2():
     sc1=Script1()
     resultado=sc1.funcion1()
-    return render_template('inicio.html', resultado=resultado)
+    return render_template('script2.html', resultado=resultado)
 
-@app.route('/script2formu')
-def script2formu():
-    sc1 = Script1()
-    resultado = sc1.funcion1()
-    return render_template('inicio.html', resultado=resultado)
+@app.route('/script2resultado',methods=['POST','GET'])
+def script2resultado():
+    email = request.form['email']
+    return render_template('script2resultado.html', email=email)
 
 
 @app.route('/script3')
